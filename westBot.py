d4bf903c76cb35ddbb87c142b4705edb43ab318f
skmartinelli <53798189+skmartinelli@users.noreply.github.com>
2020-08-14 15:29:53 -0700

    Add files via upload
    lets see if we can play mp3s

--- westBot.py
+++ westBot.py
@@ -1,10 +1,15 @@
 import discord
-from discord.ext.commands import Bot
+
+from discord.utils import get
+from discord.ext import commands
 
 
-YOURTOKEN = "NzQzOTE2NTY4MjY1ODgzODAw.XzboSw.vS71tPOMepkoNdb--NSdJWcoCpY"
+YOURTOKEN = "NzQzOTE2NTY4MjY1ODgzODAw.XzboSw.qRX0rkF_0hdjBWpmYUg1bvyx2gc"
 
 client = discord.Client()
+
+
+
 
 @client.event
 async def on_ready():
@@ -15,5 +20,24 @@ async def on_message(message):
     #message stuff
     if "!greeting" in message.content:
         await message.channel.send("howdy!")
+
+    if "!showdown" in message.content:
+        user = context.message.author
+        voice_channel = user.voice.voice_channel
+        channel = None
+        if voice_channel != None
+            channel = voice_channel.name
+            await client.say("User is in the channel: " + channel)
+            vc = await client.join)voice_channel(voice_channel)
+            player = vc.create_ffmpeg_player('goodbadugly.mp3', after=lambda: print('done'))
+            player.start()
+            while not player.isdone():
+                await asyncio.sleep(1)
+            player.stop()
+            await vc.disconnect()
+        else:
+            await client.say("User is not in a voice channel")
+
+
 
 client.run(YOURTOKEN)
