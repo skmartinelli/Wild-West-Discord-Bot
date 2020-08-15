e2c925d4485a16f1fcfb7f56598f7bd9df7815f0
skmartinelli <skm010@ucsd.edu>
2020-08-15 21:39:13 +0000

    id be happy submitting but id like to add some polish later tonight

--- westBot.py
+++ westBot.py
@@ -10,11 +10,22 @@ from discord.ext import commands
 YOURTOKEN = "NzQzOTE2NTY4MjY1ODgzODAw.XzboSw.qRX0rkF_0hdjBWpmYUg1bvyx2gc"
 
 client = discord.Client()
-
-
+vc = None
+duelActive = False
+postduel = False
+isitmusic = False
+timer = 0
 @client.event
 async def on_ready():
+    global timer
+    global postduel
     print("Bot is ready")
+    while True:
+        timer -= 1
+        await asyncio.sleep(1)
+        if timer <= 0:
+            postduel = False
+
 
 @client.event
 async def on_message(message):
@@ -24,43 +35,48 @@ async def on_message(message):
 
     if "!showdown" in message.content:
         global vc
+        global duelActive
+        global isitmusic
         #startcase = random.randint(0,2)
-        # XXX
-        startcase = 0
+        startcase = 1
+        duelActive = True
+        isitmusic = True
         user = message.author
         voice_channel = user.voice.channel
         channel = None
         if voice_channel != None:
             channel = voice_channel.name
             await message.channel.send("User is in the channel: " + channel)
-            vc = await voice_channel.connect()
+            
+            if not vc:
+                vc = await voice_channel.connect()
             
             
             if startcase == 0:
-                endDistances = [145, 166, 294.47, 434]
+                endDistances = [144.8, 166, 294.47]
                 vc.play(discord.FFmpegPCMAudio('goodbadugly.mp3'), after=lambda e: print('done', e))
                 
 
-                #length = random.choice(endDistances)
-                length = 144.8
+                length = random.choice(endDistances)
+                
                 await asyncio.sleep(length)
                 
                 
                 
                 vc.stop()
-                await vc.disconnect()
+                #await vc.disconnect()
 
             
             elif startcase == 1:
                 
-                endDistances = [150, 306]
+                endDistances = [150, 303]
                 vc.play(discord.FFmpegPCMAudio('goodbadugly225.mp3'), after=lambda e: print('done', e))
                 
                 length = random.choice(endDistances)
                 await asyncio.sleep(length)
                 
                 vc.stop()
-                await vc.disconnect()
+                #await vc.disconnect()
         
             
             elif startcase == 2:
@@ -71,17 +87,48 @@ async def on_message(message):
                 
                 
                 vc.stop()
-                await vc.disconnect()        
+                #await vc.disconnect()    
+
+
         # If they aren't in a vc
         else:
             await client.say("User is not in a voice channel")
 
-    if "bang" in message.content:
+    if "!bang" in message.content:
         global vc
-        if vc.is_playing():
-            await message.channel.send("You fired too early and lost the duel, you suck")
-        else:
-            await message.channel.send("You won the duel :D")
+        global duelActive
+        global postduel
+        global isitmusic
+        global timer
+        
+        if duelActive: 
+            if vc.is_playing() and isitmusic == True:
+                await message.channel.send("You fired too early and lost the duel, you suck")
+               
+            else: #if vc is not playing, duel is either HAPPENING, hasn't started, or has already ended
+                await message.channel.send("You won the duel :D")
+                vc.play(discord.FFmpegPCMAudio('bang.mp3'), after=lambda e: print('done', e))
+                isitmusic == False
+                vc.stop()
+                duelActive = False
+                postduel = True
+                timer = 5
+        elif not duelActive and postduel == False:
+            await message.channel.send("There's no duel happening!")
+            vc.play(discord.FFmpegPCMAudio('bang.mp3'), after=lambda e: print('done', e))
+            isitmusic == False
+        elif not duelActive and  postduel == True:
+            await message.channel.send("You lost the duel")
+            vc.play(discord.FFmpegPCMAudio('bang.mp3'), after=lambda e: print('done', e))
+            isitmusic == False
+                
+            
+            #await vc.disconnect()
+    
+    
+    if "!disconnectWest" in message.content:
+        await vc.disconnect()
+    
     
     if "!moo" in message.content:
         # Set the index and the cutoff
@@ -93,7 +140,7 @@ async def on_message(message):
         channel = None
         
         # Connect to the voice channel
-        if voice_channel != None:
+        if voice_channel is not None:
             channel = voice_channel.name
             await message.channel.send("User is in the channel: " + channel)
             vc = await voice_channel.connect()
@@ -102,9 +149,9 @@ async def on_message(message):
             await asyncio.sleep(cutoff)
             
             vc.stop()
-            await vc.disconnect()
+            #await vc.disconnect()
         else:
-            await client.say("User is not in a voice channel")
+            await message.channel.send("User is not in a voice channel")
 
 
 client.run(YOURTOKEN)
